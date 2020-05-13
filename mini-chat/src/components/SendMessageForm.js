edb58e0b8798662fed463116966724555e8b6083
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-05-12 20:36:08 -0700

    SendMessageForm component added

--- mini-chat/src/components/SendMessageForm.js
+++ mini-chat/src/components/SendMessageForm.js
@@ -1,8 +1,34 @@
 
 class SendMessageForm extends React.Component{
+   
+    handleChange(e) {
+        
+        constructor() {
+            super()
+            this.state = {
+               message: ''
+            }
+            this.handleChange = this.handleChange.bind(this)
+        }
+
+
+        this.setState({
+          message: e.target.value
+        })
+      }
+   
+   
     render(){
         return(
-            <form className= "send-mes"
+            <form className= "send-message-form">
+                <input 
+                onChange ={this.handleChange}
+                value = {this.state.message}
+                placeholder="type the message and hit enter"
+                type="text"
+                />
+            </form>
         )
     }
 }
+
