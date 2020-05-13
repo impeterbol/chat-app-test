4e19eed9f1adfeb5835350095796f8f3b96747d3
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-05-12 20:51:44 -0700

    adding for future updates

--- mini-chat/src/components/Chat.js
+++ mini-chat/src/components/Chat.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title'
 
 class Chat extends React.Component{
 constructor(){
     super()
     this.state={
         messages: DUMMY_DATA
-    }
-}
-    render(){
-        return(
-            <div className='chat'>
-                 <Title/>
-      <MessageList messages={this.state.messages}/>
-      <SendMessageForm/>
-      
-
-            </div>
-        )
     }
 }
 
@@ -28,32 +17,55 @@ componentDidMount(){
             url:testToken
         })
     })
-}
 
 chatManager.connect()
-    .then (currentUser =>{
-        currentUser.subscribeToRoom({
-            roomId:roomId,
-            hooks:{
-                oneNewMessage: message => {
-                    this.setState({
-                        messages:[...this.state.messages, message]
-                    })
-                }
+.then (currentUser =>{
+    currentUser.subscribeToRoom({
+        roomId:roomId,
+        hooks:{
+            oneNewMessage: message => {
+                this.setState({
+                    messages:[...this.state.messages, message]
+                })
             }
-        })
+        }
     })
+})
+}
+
+
+sendMessage(text) {
+    this.currentUser.sendMessage({
+      text,
+      roomId: roomId
+    })
+  }
+    render(){
+        return(
+            <div className='chat'>
+                 <Title/>
+      <MessageList messages={this.state.messages}/>
+      <SendMessageForm sendMessage = {this.sendMessage}/>
+      
+
+            </div>
+        )
+    }
+}
+
+
+
 
 
 //dummy data to check the data flow
 
-const DUMMY_DATA = [
-    {
-      senderId: "perborgen",
-      text: "who'll win?"
-    },
-    {
-      senderId: "janedoe",
-      text: "who'll win?"
-    }
-  ]
+// const DUMMY_DATA = [
+//     {
+//       senderId: "perborgen",
+//       text: "who'll win?"
+//     },
+//     {
+//       senderId: "janedoe",
+//       text: "who'll win?"
+//     }
+//   ]
---
+++ mini-chat/src/components/Title.js
@@ -0,0 +1,8 @@
+
+
+
+function Title() {
+    return <p class="title">My awesome chat app</p>
+  }
+
+  export default Title
